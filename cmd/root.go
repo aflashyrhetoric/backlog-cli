2e0fa5fac9d5d69b7ea4a58fcdc823abaecbfd92
Kevin Oh <kevin@nulab-inc.com>
2017-08-15 19:46:17 -0400

    Hid the configuration file

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,3 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -59,6 +45,12 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// BACKLOG_API_KEY
+	key := viper.GetString("BACKLOG_API_KEY")
+	RootCmd.PersistentFlags().StringVar(&cfgFile, key, "", "The Backlog API Key.")
+
+	fmt.Println(RootCmd.PersistentFlags().Lookup(key))
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -79,11 +71,5 @@ func initConfig() {
 		if err := viper.ReadInConfig(); err == nil {
 			fmt.Println("Using config file:", viper.ConfigFileUsed())
 		}
-
-		key := viper.GetString("BACKLOG_API_KEY")
-		noprefixkey := viper.GetString("API_KEY")
-
-		fmt.Println(key)
-		fmt.Println(noprefixkey)
 	}
 }
