d24f42e4536ee1b7d654085b8c6693334448b1b9
Kevin Oh <aflashyrhetoric@gmail.com>
2017-08-21 20:16:57 -0400

    Got rid of some cruft

--- cmd/me.go
+++ cmd/me.go
@@ -1,15 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-	"net/http"
-	// "net/url"
-	// "encoding/json"
-	"io/ioutil"
-	// "strings"
+"fmt"
+"net/http"
 
-	"github.com/spf13/cobra"
-	// "github.com/spf13/viper"
+"github.com/spf13/cobra"
+"github.com/spf13/viper"
 )
 
 var httpClient *http.Client
@@ -17,39 +13,18 @@ var httpClient *http.Client
 var meCmd = &cobra.Command{
 	Use:   "me",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `to quickly create a Cobra application.`,
 
 	Run: func(cmd *cobra.Command, args []string) {
+
 		apiUrl:= "/api/v2/users/myself"
 		endpoint:= Endpoint(apiUrl)
 
-		resp, err := http.Get(endpoint)
-		if err != nil {
-			fmt.Println("There's been a fatal error.")
-		}
-		
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-
-		fmt.Println(body)
+		fmt.Println(endpoint)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(meCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// meCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// meCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.Prep()
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
+"fmt"
+"os"
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+"github.com/spf13/cobra"
+"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -15,14 +15,7 @@ var apiKey string
 var RootCmd = &cobra.Command{
 	Use:   "backlog-cli",
 	Short: "Use Backlog from the command line.",
-	Long: `A longer description that spans multiple lines and likely contains
-	examples and usage of using your application. For example:
-
-	Cobra is a CLI library for Go that empowers applications.
-	This application is a tool to generate the needed files
-	to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Long: `to quickly create a Cobra application.`,
 	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
@@ -37,41 +30,29 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	// RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.backlog-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
-
-
-	// fmt.Println(RootCmd.PersistentFlags().Lookup(key))
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
 		fmt.Println("Config found. Loading...")
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
 		fmt.Println("Config not found. Setting defaults...")
 
-		// Search config in home directory with name ".backlog-cli" (without extension).
 		viper.SetConfigName(".backlog-cli")
 		viper.AddConfigPath(".")
 		viper.SetConfigType("yaml")
 		// viper.SetEnvPrefix("backlog")
-		viper.AutomaticEnv() // read in environment variables that match
 
-		// If a config file is found, read it in.
-		if err := viper.ReadInConfig(); err == nil {
-			// fmt.Println("Using config file:", viper.ConfigFileUsed())
-		}
+		// read in environment variables that match
+		viper.AutomaticEnv() 
+	}
+}
+
+func Prep() {
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
 
