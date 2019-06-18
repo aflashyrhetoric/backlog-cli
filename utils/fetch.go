2a80be22e68717b9c0522b8d975b316e88897489
KEVINOH <aflashyrhetoric@gmail.com>
2019-06-17 23:57:58 -0400

    #21 Add GetParams

--- utils/fetch.go
+++ utils/fetch.go
@@ -41,6 +41,30 @@ func Get(endpoint string) []byte {
 	return responseData
 }
 
+// GetParams .. issues an HTTP Get request with parameters
+func GetParams(endpoint string, queryParams map[string]int) []byte {
+	queryString := ""
+
+	for k, v := range queryParams {
+		queryString = fmt.Sprintf("%s&%s=%d", queryString, k, v)
+	}
+
+	endpoint = endpoint + queryString
+
+	response, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	// Response
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return responseData
+}
+
 // Post .. issues an HTTP POST request
 func Post(endpoint string, form url.Values) ([]byte, error) {
 
